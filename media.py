270c7ab66c2fd8c711829f38510cd282c7075493
realRk <rakeshramesh2000@gmail.com>
2017-06-21 15:53:33 +0530

    Update media.py

--- media.py
+++ media.py
@@ -1,5 +1,5 @@
 class Movies():
-    def __init__(self,movie_name,movie_story,movie_poster,movie_trailer):
+    def __init__(self, movie_name, movie_story, movie_poster, movie_trailer):
         self.title = movie_name
         self.story = movie_story
         self.poster_image_url = movie_poster
